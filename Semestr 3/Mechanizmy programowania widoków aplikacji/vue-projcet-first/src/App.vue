fbc164d217093eb8dba966722099138b39317392
Dawid Garncarek <150728883+DawidGarncarek@users.noreply.github.com>
2025-10-11 13:39:56 +0200

    Add files via upload

---
+++ Semestr 3/Mechanizmy programowania widoków aplikacji/vue-projcet-first/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+const name = "Dawid Garncarek"
+const subject = "Programowanie aplikacji webowych"
+</script>
+
+<template>
+  <div id="app">
+    <h1>Witaj, {{ name }}!</h1>
+    <p>Temat zajęć: {{ subject }}</p>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
